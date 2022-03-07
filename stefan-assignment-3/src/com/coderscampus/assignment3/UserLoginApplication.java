856cc729b81bda49209ef37e19d11d7943275ac3
SteveDutch <steholl@posteo.org>
2022-03-07 22:38:01 +0100

    rename main class

--- stefan-assignment-3/src/com/coderscampus/assignment3/UserLoginApplication.java
+++ stefan-assignment-3/src/com/coderscampus/assignment3/UserLoginApplication.java
@@ -3,7 +3,7 @@ package com.coderscampus.assignment3;
 public class UserLoginApplication {
 
 	public static void main(String[] args) {
-		UsersData data = new UsersData();
+		UserService data = new UserService();
 		System.out.println(data);
 		System.out.println("fini");
 
